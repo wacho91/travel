934cf1a82e1f5ff372e841ff3bc5a38e0e8bcea6
wacho91 <criskol.71@gmail.com>
2024-09-03 11:45:19 -0500

    Navabr Component Complete

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/logo.png"
 const Navbar = () => {
   return (
     <>
-        <nav className="container flex flex-row justify-around items-center">
+        <nav className="container flex flex-row justify-around items-center md:py-8 uppercase font-semibold relative z-20 text-xs md:text-lg">
             <a href="#">About</a>
             <a href="#">Explore</a>
             <a href="#"><img src={Logo} alt="" className="w-14"/></a>
