ea4cf9de16e90ea263a365da3995b5bec1550f89
wacho91 <criskol.71@gmail.com>
2024-09-05 10:19:11 -0500

    Hero complete with animation

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 
 import { FaPlay } from "react-icons/fa"
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -16,13 +17,13 @@ const Hero = () => {
             </div>
 
             {/*Hero Text Section*/}
-            <div className="container relative z-20 text-center space-y-8">
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.5, delay: 0.5 }} className="container relative z-20 text-center space-y-8">
                 <h1 className="text-3xl md:text-5xl font-bold font-merriweather">The Great Outdoors</h1>
                 <p>Wonder often, wonder Always.</p>
                 <div className="inline-flex justify-center items-center border-2 h-14 w-14 rounded-full !mt-12 md:!mt-16">
                     <FaPlay />
                 </div>
-            </div>
+            </motion.div>
         </section>   
     </>
   )
